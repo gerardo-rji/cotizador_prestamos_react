e11dd55c631ca6fb7a79553a48f323ca72b3604f
Gerardo_RJI <gera.rji96@gmail.com>
2026-05-15 06:07:12 -0600

    Create a slider for amount selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [cantidad, setCantidad] = useState(10000);
+
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  function handleChange(e) {
+    setCantidad(+e.target.value);
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header/>
+
+      <input
+      type="range"
+      className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      onChange={handleChange}
+      min={MIN}
+      max={MAX}
+      step={STEP}
+      value={cantidad}
+      />
+
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {cantidad}
+      </p>
     </div>
   )
 }
